10093abb787dc509f7d32086541830368a4e9d97
André Fernandes <andre.fernandes@VAKT.io>
2025-12-16 15:16:52 +0000

    Update deploy yml

--- apps/web-client/src/app/data/config.ts
+++ apps/web-client/src/app/data/config.ts
@@ -5,5 +5,5 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const AppConfig = {
   API_URL,
   TEAM_NAME: process.env.NEXT_PUBLIC_TEAM_NAME ?? 'Team Name',
-  TEAM_COLOR: process.env.NEXT_PUBLIC_TEAM_COLOR ?? 'green',
+  TEAM_COLOR: process.env.NEXT_PUBLIC_TEAM_COLOR ?? '#green',
 };
